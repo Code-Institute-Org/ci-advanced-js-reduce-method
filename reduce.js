abd5fc3210cdf1d46f1b6e259ff641155707ed91
Chris Z <ckz8780@gmail.com>
2020-12-17 02:36:35 +0000

    created reduce.js

---
+++ reduce.js
@@ -0,0 +1,30 @@
+// Summing an array of numbers:
+
+
+const teamMembers = [
+  {
+    name: 'Andrew',
+    profession: 'Developer',
+    yrsExperience: 5
+  },
+  {
+    name: 'Ariel',
+    profession: 'Developer',
+    yrsExperience: 7
+  },
+  {
+    name: 'Michael',
+    profession: 'Designer',
+    yrsExperience: 1
+  },
+  {
+    name: 'Kelly',
+    profession: 'Designer',
+    yrsExperience: 3
+  }
+];
+
+// Totaling a specific object property
+
+
+// Grouping by a property, and totaling it too
